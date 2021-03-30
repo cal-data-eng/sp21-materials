226ec09f0853fd60f37c8229274b702f5f1974e6
Allen Shen <allenshen5@berkeley.edu>
2021-03-30 04:28:25 -0700

    Update Project 3

--- proj/proj3/tests/q3b.py
+++ proj/proj3/tests/q3b.py
@@ -1,7 +1,7 @@
 test = {   'name': 'q3b',
     'points': 1,
     'suites': [   {   'cases': [   {'code': ">>> 0 <= pd.read_csv('results/result_3b.csv').iloc[0, 0] <= 50\nTrue", 'hidden': False, 'locked': False},
-                                   {'code': ">>> # TEST;\n>>> 0 <= pd.read_csv('results/result_3b.csv').iloc[0, 1] <= 50\nTrue", 'hidden': False, 'locked': False}],
+                                   {'code': ">>> 0 <= pd.read_csv('results/result_3b.csv').iloc[0, 1] <= 50\nTrue", 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
