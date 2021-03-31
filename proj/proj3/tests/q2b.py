53a8cf81d1e11b51e80245c1707d27524201581d
Allen Shen <allenshen5@berkeley.edu>
2021-03-31 07:37:05 -0700

    Update Project 3 q2b tests

--- proj/proj3/tests/q2b.py
+++ proj/proj3/tests/q2b.py
@@ -1,7 +1,7 @@
 test = {   'name': 'q2b',
     'points': 1,
     'suites': [   {   'cases': [   {'code': ">>> pd.read_csv('results/result_2b.csv').shape == (100, 7)\nTrue", 'hidden': False, 'locked': False},
-                                   {'code': ">>> np.isclose(pd.read_csv('results/result_2b.csv').iloc[:50, -1].sum(), 38323.42428000002)\nTrue", 'hidden': False, 'locked': False}],
+                                   {'code': ">>> np.isclose(pd.read_csv('results/result_2b.csv').iloc[:50, -1].sum(), 39029.071140000015)\nTrue", 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
